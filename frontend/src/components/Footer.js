d9a0cb63d4f2d471c6aefee5123840181eb859de
israfilova14 <aynurisrafilova9@gmail.com>
2024-06-28 14:39:16 +0400

    some code added

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -2,11 +2,44 @@ import React from 'react'
 
 const Footer = () => {
   return (
-     <footer className='bg-slate-200'>
-       <div className='container mx-auto p-4 mt-8'>
-         <p className='text-center font-medium'>1996-2024, Amazon.com, Inc. or its affiliates</p>
-       </div>
-     </footer>
+    <footer className='bg-slate-200 text-slate-600'>
+      <div className='container mx-auto p-3 mt-8'>
+        <div className='flex flex-col md:flex-row gap-10 mx-auto items-start md:items-center justify-around'>
+          <div className='mb-6 md:mb-0'>
+            <h2 className='font-medium'>Get to Know Us</h2>
+            <p>Careers</p>
+            <p>About Amazon</p>
+            <p>Investor Relations</p>
+            <p>Amazon Devices</p>
+          </div>
+          <div className='mb-6 md:mb-0'>
+            <h2 className='font-medium'>Make Money with Us</h2>
+            <p>Sell on Amazon</p>
+            <p>Sell Your Services on Amazon</p>
+            <p>Sell on Amazon Business</p>
+            <p>Sell Your Apps on Amazon</p>
+            <p>Become an Affiliate</p>
+          </div>
+          <div className='mb-6 md:mb-0'>
+            <h2 className='font-medium'>Amazon Payment Products</h2>
+            <p>Amazon Rewards Visa Signature Cards</p>
+            <p>Amazon.com Store Card</p>
+            <p>Amazon.com Corporate Credit Line</p>
+            <p>Shop with Points</p>
+            <p>Credit Card Marketplace</p>
+          </div>
+          <div className='mb-6 md:mb-0'>
+            <h2 className='font-medium'>Let Us Help You</h2>
+            <p>Your Account</p>
+            <p>Your Orders</p>
+            <p>Shipping Rates & Policies</p>
+            <p>Amazon Prime</p>
+            <p>Returns & Replacements</p>
+            <p>Manage Your Content and Devices</p>
+          </div>
+        </div>
+      </div>
+    </footer>
   )
 }
 
--- frontend/src/components/pages/ForgotPassword.js
+++ frontend/src/components/pages/ForgotPassword.js
@@ -1,8 +1,26 @@
 import React from 'react'
+import password from '../../assest/banner/password.png'
 
 const ForgotPassword = () => {
   return (
-    <div>ForgotPassword</div>
+    <div>
+      <div className='mt-10'>
+         <h2 className='text-center mb-2 text-2xl'>Password Manager</h2>
+         <div className='border border-b border-slate-300'></div>
+      </div>
+      <div className='mt-7 text-center'>
+        <p className='text-slate-600'>See, change, or remove passwords you  saved in your Google Account. <a className='text-blue-500'>Learn more</a></p>
+      </div>
+      <div className='flex items-center mt-9 border border-slate-300 w-[700px] h-[280px] mx-auto p-6 rounded gap-3'>
+         <div>
+            <h2>Password Checkup</h2>
+            <p className='text-slate-500'>Check your svaed passwords to strengthen your security</p>
+         </div>
+         <div>
+             <img src={password}/>
+         </div>
+      </div>
+    </div>
   )
 }
 
