953728077662e10d6e121ac904aa509902c95349
israfilova14 <aynurisrafilova9@gmail.com>
2025-01-22 17:11:38 +0400

    some changes

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const cors = require ('cors')
 require('dotenv').config()
 const cookieParser = require('cookie-parser')
+
 const connectDB = require('./config/db')
 const router = require('./routes/index')
 
@@ -11,15 +12,18 @@ app.use(cors({
     credentials: true
     
 }));
+
 app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 app.use(cookieParser())
+
 app.use("/api", router)
 
-const PORT = 8080 || process.env.PORT
-connectDB().then(() => {
+const PORT = process.env.PORT || 8080
 
-})
-app.listen(PORT, () => {
-    console.log("connect to DB");
-    console.log("Server is running");
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log("connect to DB");
+        console.log("Server is running");
+    })
 })
